b783a2c16c2ece719b0f1388c33cd7a7ddb32a69
JunryPacot <junrypacot878@gmail.com>
2025-10-18 06:12:22 +0800

    Create database and patient table with initial data

---
+++ Db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE FPT1_Pacot;
+USE FPT1_Pacot;
+
+CREATE TABLE patient_Pacot (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(100),
+  lastname VARCHAR(100),
+  gender VARCHAR(10),
+  age INT
+);
+
+INSERT INTO patient_Pacot (firstname, lastname, gender, age)
+VALUES
+('Junry', 'Pacot', 'Male', 19),
+('Lee', 'Bebis', 'Male', 19),
+('Mike', 'Ibit', 'Male', 19),
+('Jashaa', 'Deofilo', 'Female', 20),
+('Crispen', 'Duarte', 'Male', 19);
